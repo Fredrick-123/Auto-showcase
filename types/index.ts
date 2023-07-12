261bfdf077ebdaf490530fbad3249f9b30d79c0b
Fredrick-123 <87024776+Fredrick-123@users.noreply.github.com>
2023-07-12 21:06:03 +0100

    first-comment

---
+++ types/index.ts
@@ -0,0 +1,52 @@
+import { FilterProps } from "@/utils";
+import { MouseEventHandler } from "react";
+
+
+
+
+export interface CustomFilterProps {
+  title: string;
+  options: OptionProps[];
+}
+
+export interface HomeProps {
+  searchParams: FilterProps;
+}
+
+export interface OptionProps {
+  title: string;
+  value: string;
+}
+
+
+export interface customButtonProps{
+    isDisabled?: boolean;
+    btnType?: "button" | "submit";
+    containerStyles?: string;
+    textStyles?: string;
+    title: string;
+    rightIcon?: string;
+    handleClick?: MouseEventHandler<HTMLButtonElement>;
+}
+
+export interface SearchManuFacturerProps {
+    manufacturer: string;
+    setManuFacturer: (manufacturer: string) => void;
+  }
+
+
+  
+  export interface CarProps {
+    city_mpg: number;
+    class: string;
+    combination_mpg: number;
+    cylinders: number;
+    displacement: number;
+    drive: string;
+    fuel_type: string;
+    highway_mpg: number;
+    make: string;
+    model: string;
+    transmission: string;
+    year: number;
+  } 
